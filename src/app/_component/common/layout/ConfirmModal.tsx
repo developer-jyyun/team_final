2a352438dbb46c801d3681a41830784806b5c9fa
developer-jyyun <developer.jyyun@gmail.com>
2024-01-09 19:26:19 +0900

    Refactor: 모달 버튼영역에 공통 컴포넌트적용

--- src/app/_component/common/layout/ConfirmModal.tsx
+++ src/app/_component/common/layout/ConfirmModal.tsx
@@ -1,5 +1,6 @@
 import useOnClickOutside from "@/hooks/useOnClickOutside";
 import React, { useRef } from "react";
+import Button from "../atom/Button";
 
 interface Props {
   isOpen: boolean;
@@ -38,20 +39,16 @@ const ConfirmModal = ({
                 정말로 탈퇴 하시겠어요?
               </p>
               <div className="flex flex-row justify-between items-between px-[72px]">
-                <button
-                  className="text-black-4 font-medium"
-                  type="button"
-                  onClick={onConfirm}
-                >
-                  예
-                </button>
-                <button
-                  className="text-blue font-bold"
-                  type="button"
-                  onClick={onClose}
-                >
-                  아니오
-                </button>
+                <Button
+                  text="예"
+                  styleClass="text-black-4 font-medium"
+                  onClickFn={onConfirm}
+                />
+                <Button
+                  text="아니오"
+                  styleClass="text-blue font-bold"
+                  onClickFn={onClose}
+                />
               </div>
             </>
           )}
@@ -62,20 +59,17 @@ const ConfirmModal = ({
                 로그인 하시겠습니까
               </p>
               <div className="flex flex-row justify-between items-between px-[72px]">
-                <button
-                  className="text-black-4 font-medium"
-                  type="button"
-                  onClick={onClose}
-                >
-                  취소
-                </button>
-                <button
-                  className="text-blue font-bold"
-                  type="button"
-                  onClick={onConfirm}
-                >
-                  확인
-                </button>
+                <Button
+                  text="취소"
+                  styleClass="text-black-4 font-medium"
+                  onClickFn={onClose}
+                />
+
+                <Button
+                  text="확인"
+                  styleClass="text-blue font-bold"
+                  onClickFn={onConfirm}
+                />
               </div>
             </>
           )}
@@ -86,16 +80,14 @@ const ConfirmModal = ({
           className="w-[87.2%] web:w-[327px]  h-[140px] mx-auto absolute top-[30%] left-[50%] translate-x-[-50%] rounded-md bg-white flex flex-col justify-evenly"
           ref={ref}
         >
-          <p className="text-4.5 font-bold text-center">{message}</p>
+          <p className="text-4.5 font-bold text-left px-8">{message}</p>
 
           <div className="flex flex-row justify-end items-between px-8">
-            <button
-              className={`text-blue font-bold ${styleClass}`}
-              type="button"
-              onClick={onClose}
-            >
-              확인
-            </button>
+            <Button
+              text="확인"
+              styleClass={`text-blue font-bold ${styleClass}`}
+              onClickFn={onClose}
+            />
           </div>
         </div>
       )}
