876cd05fc3c90ba2d0680de237578de06499b2fb
sunpaaaa <sunpaaaa@gmail.com>
2024-01-10 11:45:12 +0900

    Refactor: Empty 컴포넌트 이미지 변경 가능하도록 변경

--- src/app/_component/common/layout/EmptyBox.tsx
+++ src/app/_component/common/layout/EmptyBox.tsx
@@ -1,17 +1,18 @@
 import React from "react";
 
 interface Props {
-  text: string;
+  text?: string;
+  imgUrl?: string;
   children?: React.ReactNode;
 }
-const EmptyBox = ({ text, children }: Props) => {
+const EmptyBox = ({
+  text,
+  imgUrl = "/assets/sadMainLogo.svg",
+  children,
+}: Props) => {
   return (
     <div className="flex flex-col justify-center items-center">
-      <img
-        src="./assets/sadMainLogo.svg"
-        alt="슬퍼하는 로고"
-        className="m-10 w-[160px] h-[132px]"
-      />
+      <img src={imgUrl} alt="로고" className="m-10 w-[160px] h-[132px]" />
       {children}
       <p className="text-grey-4 text-center text-sm font-normal tracking-tighter">
         {text}
