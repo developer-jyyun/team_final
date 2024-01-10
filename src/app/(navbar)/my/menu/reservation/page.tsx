5d98fb5d39d55bffe7a80d01165b381d49e44e0a
developer-jyyun <developer.jyyun@gmail.com>
2024-01-10 17:54:09 +0900

    Design: 예약내역 스타일링 수정

--- src/app/(navbar)/my/menu/reservation/page.tsx
+++ src/app/(navbar)/my/menu/reservation/page.tsx
@@ -4,9 +4,9 @@ import MenuSection from "../_component/MenuSection";
 // TODO:: 여백 색상 상세 디자인 확정 시 작업
 const ReservationPage = () => {
   return (
-    <MenuSection styleClass="w-[95%]" text="예약 내역" backUrl="/my">
-      <h2 className="font-bold">
-        총 <span className="text-pink ">N</span>
+    <MenuSection text="예약 내역" backUrl="/my">
+      <h2 className="font-bold text-black-2 text-lg mb-8">
+        총 <span className="text-pink-main ">N</span>
         개의 패키지 상품
       </h2>
       <ul>
