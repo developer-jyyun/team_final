88272211a1daefd772ac8ca25f16fb4b8156b671
developer-jyyun <developer.jyyun@gmail.com>
2024-01-12 10:26:45 +0900

    Feat: 리뷰 작성 페이지 별점 계산 및 렌더링 구현

---
+++ src/app/(navbar)/my/review/_components/ReviewScore.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import React, { useEffect, useState } from "react";
+import RangeSlider from "./RangeSlider";
+import StarScore from "./StarScore";
+
+const ReviewScore = () => {
+  const [productScore, setProductScore] = useState(0);
+  const [scheduleScore, setScheduleScore] = useState(0);
+  const [friendlinessScore, setFriendlinessScore] = useState(0);
+  const [appointmentScore, setAppointmentScore] = useState(0);
+
+  const [averageScore, setAverageScore] = useState(0);
+
+  useEffect(() => {
+    const calculateScore = () => {
+      return (
+        (productScore + scheduleScore + friendlinessScore + appointmentScore) /
+        4
+      );
+    };
+    setAverageScore(calculateScore());
+  }, [productScore, scheduleScore, friendlinessScore, appointmentScore]);
+
+  return (
+    <div>
+      <StarScore averageScore={averageScore} />
+      <ul className="flex flex-col gap-4 px-1 py-4 mt-6">
+        <li className="relative flex justify-between items-center">
+          <span className="text-black-4 font-semibold text-xs">상품구성</span>
+          <div className="w-2/3   absolute left-[25%]">
+            <RangeSlider
+              value={productScore}
+              onChange={setProductScore}
+              max={5}
+              min={0}
+              step={1}
+            />
+          </div>
+          <span className="text-black-6 font-semibold text-xxs">
+            {productScore}
+          </span>
+        </li>
+
+        <li className="relative flex justify-between items-center">
+          <span className="text-black-4 font-semibold text-xs">일정구성</span>
+          <div className="w-2/3  absolute left-[25%]">
+            <RangeSlider
+              value={scheduleScore}
+              onChange={setScheduleScore}
+              max={5}
+              min={0}
+              step={1}
+            />
+          </div>
+          <span className="text-black-6 font-semibold text-xxs">
+            {scheduleScore}
+          </span>
+        </li>
+        <li className="relative flex justify-between items-center">
+          <span className="text-black-4 font-semibold text-xs">
+            가이드친절도
+          </span>
+          <div className="w-2/3 absolute left-[25%]">
+            <RangeSlider
+              value={friendlinessScore}
+              onChange={setFriendlinessScore}
+              max={5}
+              min={0}
+              step={1}
+            />
+          </div>
+          <span className="text-black-6 font-semibold text-xxs">
+            {friendlinessScore}
+          </span>
+        </li>
+        <li className="relative flex justify-between items-center">
+          <span className="text-black-4 font-semibold text-xs">
+            가이드 시간
+            <br />
+            일정준수
+          </span>
+          <div className="w-2/3 absolute left-[25%]">
+            <RangeSlider
+              value={appointmentScore}
+              onChange={setAppointmentScore}
+              max={5}
+              min={0}
+              step={1}
+            />
+          </div>
+          <span className="text-black-6 font-semibold text-xxs">
+            {appointmentScore}
+          </span>
+        </li>
+      </ul>
+    </div>
+  );
+};
+
+export default ReviewScore;
---
+++ src/app/(navbar)/my/review/page.tsx
@@ -0,0 +1,17 @@
+import InnerSection from "../_component/InnerSection";
+import ReservationItem from "../_component/ReservationItem";
+import ReviewScore from "./_components/ReviewScore";
+import WriteReview from "./_components/WriteReview";
+
+const WriteReviewPage = () => {
+  return (
+    <InnerSection text="리뷰 작성하기" backUrl="/my">
+      <h2 className="font-bold">패키지 정보</h2>
+      <ReservationItem theme="review" hashTag />
+      <ReviewScore />
+      <WriteReview />
+    </InnerSection>
+  );
+};
+
+export default WriteReviewPage;
