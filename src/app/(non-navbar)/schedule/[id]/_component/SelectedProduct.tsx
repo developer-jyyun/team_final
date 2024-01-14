a57c1f5343de132a56093e0d1e59b9e1c6a3c5d1
Yonghun <yonghuni484@gmail.com>
2024-01-14 20:20:16 +0900

    Feat: 선택된 상품 데이터 연결

--- src/app/(non-navbar)/schedule/[id]/_component/SelectedProduct.tsx
+++ src/app/(non-navbar)/schedule/[id]/_component/SelectedProduct.tsx
@@ -5,6 +5,10 @@ import useScheduleDateStore from "@/store/useScheduleDateStore";
 const SelectedProduct = () => {
   const scheduleDate = useScheduleDateStore();
 
+  const formatSelectedItemDate = (date: string) => {
+    return date.replace(/-/g, ".");
+  };
+
   if (scheduleDate.data === null) {
     return (
       <div className="flex flex-col justify-end grow -translate-y-16 web:-translate-y-11">
@@ -15,7 +19,7 @@ const SelectedProduct = () => {
           <div className="w-[60px] h-[60px] mr-[14px] bg-grey-d rounded-md" />
           <div className="flex flex-col justify-center">
             <div className="w-[180px] h-[25px] bg-grey-d rounded-md mb-1" />
-            <div className="w-[180px] h-[25px] bg-grey-d rounded-md" />
+            <div className="w-[190px] h-[25px] bg-grey-d rounded-md" />
           </div>
         </div>
       </div>
@@ -34,12 +38,15 @@ const SelectedProduct = () => {
         <div className="flex flex-col justify-center">
           <div>
             <span className="text-black-2 font-medium mr-3 web:text-lg">
-              2023.12.26
+              {formatSelectedItemDate(scheduleDate.data.departureDatetime.date)}
             </span>
-            <span className="text-black-4 text-xs web:text-sm">18:28 출발</span>
+            <span className="text-black-4 text-xs web:text-sm">
+              {scheduleDate.data.departureDatetime.time} 출발
+            </span>
           </div>
           <div className="text-pink-main text-lg font-semibold web:text-xl">
-            {scheduleDate.data.totalPrice.adult.toLocaleString()}원 (3박 4일)
+            {scheduleDate.data.totalPrice.adult.toLocaleString()}원 (
+            {scheduleDate.data.lodgeDays}박 {scheduleDate.data.tripDays}일)
           </div>
         </div>
       </div>
