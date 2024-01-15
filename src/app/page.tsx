a5e0db524cd90c453ce8cfacb07b84a7895df1d6
applevalley <skatkd7571@naver.com>
2024-01-16 01:34:25 +0900

    feat: 테마 패키지 목록 조회 API 연결

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,20 +5,23 @@ import HomeHashtags from "./_component/home/HomeHashtags";
 import HomePackages from "./_component/home/HomePackages";
 import HomeProsAndCons from "./_component/home/HomeProsAndCons";
 import HomeThemePackage from "./_component/home/HomeThemePackage";
+import ContentsContainer from "./_component/common/layout/ContentsContainer";
 
 const Home = async () => {
   return (
     <>
       <main>
-        <section className="w-full flex flex-col items-center">
+        <section className="w-full">
           <DefaultHeader text="/" theme="main" />
-          <div className="w-[327px] web:w-[90%] [&>*:nth-child(n)]:mb-10">
+          <div className="flex flex-col items-center [&>*:nth-child(n)]:mb-10">
             {/* 광고구좌 */}
             <HomeAdvertisements />
             {/* 해시태그 검색 */}
             <HomeHashtags />
             {/* 패키지 컨셉 */}
-            <HomeThemePackage />
+            <ContentsContainer title="여행 테마 패키지">
+              <HomeThemePackage />
+            </ContentsContainer>
             {/* 찬반토론 */}
             <HomeProsAndCons />
             {/* 초특가 패키지 목록 */}
