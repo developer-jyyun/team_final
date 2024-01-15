7457e7883bec7692d6b5ea0f598caedeaf7775bc
applevalley <skatkd7571@naver.com>
2024-01-15 13:06:31 +0900

    design: 해시태그 레이아웃 / 메인 페이지 레이아웃 수정

--- src/app/_component/home/HomeHashtags.tsx
+++ src/app/_component/home/HomeHashtags.tsx
@@ -4,10 +4,10 @@ import Button from "@/app/_component/common/atom/Button";
 
 const HomeHashtags = () => {
   return (
-    <div className="w-[90%]">
+    <div className="w-[100%]">
       <Button
         text="3초만에 내가 원하는 여행 찾기 >"
-        styleClass="w-full h-[56px] p-4 hover:bg-[#bb1e4a] bg-pink text-white text-left tracking-widest rounded-lg"
+        styleClass="w-[327px] web:w-[100%] h-[64px] pl-5 py-5 hover:bg-[#bb1e4a] bg-pink text-white text-left tracking-widest rounded-lg"
       />
     </div>
   );
