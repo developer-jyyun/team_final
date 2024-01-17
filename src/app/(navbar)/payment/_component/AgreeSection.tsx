2c7e1c9ed3681e12b0efd9b231de8309e6293be6
Leejinuk <leejin7900@gmail.com>
2024-01-17 16:02:33 +0900

    Fix: 코드 수정

---
+++ src/app/(navbar)/payment/_component/AgreeSection.tsx
@@ -0,0 +1,56 @@
+import React, { ReactNode } from "react";
+import Image from "next/image";
+import useBottomScrollListener from "../hooks/useBottomScrollListener";
+import BottomScroll from "./BottomScroll";
+
+interface Props {
+  title: string;
+  defaultSrc: string;
+  activeSrc: string;
+  isActive: boolean;
+  onSectionChange: (isActive: boolean) => void;
+  children?: ReactNode;
+}
+
+const AgreeSection = ({
+  title,
+  defaultSrc,
+  activeSrc,
+  isActive,
+  onSectionChange,
+  children,
+}: Props) => {
+  const { bottomScrollRef, imageSrc, isDivHidden } = useBottomScrollListener(
+    defaultSrc,
+    activeSrc,
+  );
+
+  return (
+    <>
+      <div className="flex bg-pink-transparent-2 p-2 my-2 rounded-lg relative">
+        <Image src={imageSrc} alt="체크 전 이미지" width={24} height={24} />
+        <p className="ml-2 text-black-4 text-base font-normal">{title}</p>
+        <div className="absolute right-4">
+          <Image
+            src="/icons/rightArrowIcon.svg"
+            alt="우측 화살표"
+            width={24}
+            height={24}
+          />
+        </div>
+      </div>
+      <div
+        className={`h-52 bg-grey-transparent rounded-lg p-3.5 overflow-y-scroll ${
+          isDivHidden ? "hidden" : ""
+        }`}
+      >
+        {children}
+        <div ref={bottomScrollRef}>
+          <BottomScroll />
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default AgreeSection;
