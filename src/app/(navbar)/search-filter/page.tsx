a143b185c943c8d53f8f42dfeb0d43a64e743975
sunpaaaa <sunpaaaa@gmail.com>
2024-01-17 17:54:52 +0900

    Feat: 300_해시태그 페이지 생성

--- src/app/(navbar)/search-filter/page.tsx
+++ src/app/(navbar)/search-filter/page.tsx
@@ -1,7 +1,40 @@
+"use client";
+
+import ContentsContainer from "@/app/_component/common/layout/ContentsContainer";
+import DefaultHeader from "@/app/_component/common/layout/DefaultHeader";
 import React from "react";
+import ConceptsList from "./_component/ConceptsList";
+import CategoriesList from "./_component/CategoriesList";
+import RangeSlider from "../my/review/_components/RangeSlider";
+import SearchButton from "./_component/SearchButton";
 
 const SearchFilterPage = () => {
-  return <div>SearchFilterPage</div>;
+  return (
+    <section className="w-full flex flex-col items-center">
+      <DefaultHeader text="내가 원하는 여행 찾기" />
+      <ContentsContainer title="여행 예산부터!">
+        <RangeSlider
+          onChange={() => {}}
+          value={200}
+          max={500}
+          min={0}
+          step={50}
+        />
+      </ContentsContainer>
+      <ContentsContainer title="어떤 컨셉으로 놀까?">
+        <ConceptsList />
+      </ContentsContainer>
+      <ContentsContainer title="어디로 갈까?">
+        <CategoriesList type="nation" />
+        <CategoriesList type="continent" />
+      </ContentsContainer>
+      <SearchButton disabled={false}>
+        <p className="font-medium text-lg">
+          검색된 <span className="font-extrabold">{"N"}</span>개의 상품 보기
+        </p>
+      </SearchButton>
+    </section>
+  );
 };
 
 export default SearchFilterPage;
