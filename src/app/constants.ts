9e2661f6da81f3a97873c9df3e5bf5c3b97d8cce
applevalley <skatkd7571@naver.com>
2024-01-18 04:20:53 +0900

    refactor: 메인 초특가 패키지 디자인 개선

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -8,3 +8,15 @@ export const MAX_NUM_OF_COL = 5;
 
 // my page
 export const TAB_PAGE_SIZE = 3;
+
+// home page
+export const NATION_LISTS = [
+  "전체",
+  "일본",
+  "동남아",
+  "유럽",
+  "오세아니아",
+  "북미",
+  "중동",
+  "남미",
+];
