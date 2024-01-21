04326fe51bea9fdaf7b7f1fcdce1944e24c4744e
Leejinuk <leejin7900@gmail.com>
2024-01-22 00:11:10 +0900

    Fix 코드수정

--- src/app/(navbar)/compare/_component/MoreButton.tsx
+++ src/app/(navbar)/compare/_component/MoreButton.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const MoreButton = () => {
   return (
-    <div className="flex justify-center  items-center mt-2">
+    <div className="flex items-center justify-center h-20 w-full bg-gradient-white web:h-20">
       <button
         type="button"
         className="py-1 px-2 border-[0.6px] border-grey-a rounded-full bg-white"
