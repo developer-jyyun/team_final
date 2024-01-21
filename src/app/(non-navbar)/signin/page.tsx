1b26c7c01d632e1010bc7d9710c23e8d53b76e3d
Yonghun <yonghuni484@gmail.com>
2024-01-22 02:01:27 +0900

    Fix: 로그인 메인 페이지 searchParams 타입 수정

--- src/app/(non-navbar)/signin/page.tsx
+++ src/app/(non-navbar)/signin/page.tsx
@@ -10,7 +10,7 @@ export const metadata: Metadata = {
 const SigninPage = ({
   searchParams,
 }: {
-  searchParams: { departDate: string };
+  searchParams: { redirect: string };
 }) => {
   return (
     <div className="w-full">
