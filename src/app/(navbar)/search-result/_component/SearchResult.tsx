dea9d066e8746a024c689c8658207ed31cd9787c
sunpaaaa <sunpaaaa@gmail.com>
2024-01-22 17:41:34 +0900

    Feat: 검색 결과 페이지 아이템 총 개수 표시

--- src/app/(navbar)/search-result/_component/SearchResult.tsx
+++ src/app/(navbar)/search-result/_component/SearchResult.tsx
@@ -8,6 +8,12 @@ interface Props {
 const SearchResult = ({ data }: Props) => {
   return (
     <div className="w-full">
+      <div className="py-4">
+        <p className="text-lg font-semibold leading-normal tracking-tighter">
+          <span className=" text-pink-main">{data.page.totalElements}</span>
+          개의 패키지 상품 검색 결과
+        </p>
+      </div>
       <PackagesList data={data?.packages} />
     </div>
   );
