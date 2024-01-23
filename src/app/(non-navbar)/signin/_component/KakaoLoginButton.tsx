6a6d46123359095246b4c36342fad8d34de0ad74
Yonghun <yonghuni484@gmail.com>
2024-01-23 16:00:14 +0900

    Fix: Oauth API 수정

--- src/app/(non-navbar)/signin/_component/KakaoLoginButton.tsx
+++ src/app/(non-navbar)/signin/_component/KakaoLoginButton.tsx
@@ -7,19 +7,8 @@ const KakaoLoginButton = () => {
   const handleKakaoAuth = async () => {
     await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/oauth2/authorization/kakao`,
-      {
-        mode: "no-cors",
-      },
     );
   };
-  // const handleKakaoAuth2 = async () => {
-  //   await fetch(
-  //     `${process.env.NEXT_PUBLIC_BASE_URL}/oauth2/authorization/kakao`,
-  //     {
-  //       mode: "no-cors",
-  //     },
-  //   );
-  // };
 
   return (
     <button
@@ -32,15 +21,6 @@ const KakaoLoginButton = () => {
       </div>
       <div>카카오 로그인</div>
     </button>
-    // <Link
-    //   href={`https://api.winnerone.site/oauth2/authorization/kakao`}
-    //   className="w-[327px] h-14 rounded-lg bg-yellow flex justify-center items-center text-brown web:w-[435px] web:text-lg"
-    // >
-    //   <div className="flex items-center justify-center w-14 h-14 mr-4">
-    //     <img src="/icons/kakaoLogo.svg" alt="카카오 로고" />
-    //   </div>
-    //   <div>카카오 로그인</div>
-    // </Link>
   );
 };
 
