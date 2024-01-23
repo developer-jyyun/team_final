49d084715d03e27bc895e5f42212e29d482bf3ea
Yonghun <yonghuni484@gmail.com>
2024-01-23 14:10:03 +0900

    Fix: Oauth API credentials 제거

--- src/api/signin/getNaverAuth.ts
+++ src/api/signin/getNaverAuth.ts
@@ -2,9 +2,6 @@ const getNaverAuth = async () => {
   try {
     const result = await fetch(
       `${process.env.NEXT_PUBLIC_BASE_URL}/oauth2/authorization/naver`,
-      {
-        credentials: "include",
-      },
     );
 
     const data = await result.json();
