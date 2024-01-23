8bef5f11a471550aa65e9cd4d5e51d77d59e1ae3
sunpaaaa <sunpaaaa@gmail.com>
2024-01-23 17:23:08 +0900

    Feat: 검색 결과 페이지 아이템 정렬 기능 추가

---
+++ src/app/(navbar)/search-result/_component/DropDownBox.tsx
@@ -0,0 +1,68 @@
+"use client";
+
+import { useState } from "react";
+import { SORT_OPTIONS } from "@/app/constants";
+import { useRouter, useSearchParams } from "next/navigation";
+import DropDownOption from "./DropDownOption";
+
+const DropDownBox = () => {
+  const [selected, setSelected] = useState("출발일순");
+  const [showOptions, setShowOptions] = useState(false);
+
+  const params = useSearchParams();
+  const router = useRouter();
+
+  const handleClick = () => {
+    setShowOptions((prev) => !prev);
+  };
+
+  const handleSelect = (value: string, option: string) => {
+    setSelected(value);
+    setShowOptions(false);
+
+    const oldParams = params.toString();
+    console.log(oldParams.split("&sortBy")[0]);
+
+    if (!params.get("sortBy")) {
+      router.replace(`/search-result?${oldParams}&sortBy=${option}`);
+    } else {
+      const newParams = oldParams.split("&sortBy=")[0];
+      router.replace(`/search-result?${newParams}&sortBy=${option}`);
+    }
+  };
+  return (
+    <div className="relative inline-block text-left z-10">
+      <button
+        onClick={handleClick}
+        type="button"
+        className="flex items-center font-normal py-2 px-4 text-xs text-black-4 focus:outline-none focus:shadow-outline"
+      >
+        <p>{selected}</p>
+        <img
+          src="/icons/rightArrowIcon.svg"
+          alt="더보기"
+          className="transition-transform duration-300 ease-in-out"
+          style={
+            showOptions
+              ? { transform: "rotate(-90deg)" }
+              : { transform: "rotate(90deg)" }
+          }
+        />
+      </button>
+      {showOptions && (
+        <div className="origin-top-right absolute right-0 mt-2 w-32 rounded-md overflow-hidden shadow-lg">
+          {Object.entries(SORT_OPTIONS).map(([key, value]) => (
+            <DropDownOption
+              key={key}
+              value={value}
+              option={key}
+              handleSelect={handleSelect}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default DropDownBox;
