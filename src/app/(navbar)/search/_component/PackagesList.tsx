b9aa50a62f69ecedacea0904679920396973f186
sunpaaaa <sunpaaaa@gmail.com>
2024-01-23 17:56:32 +0900

    Fix: 검색 페이지 데이터 뎁스 수정

--- src/app/(navbar)/search/_component/PackagesList.tsx
+++ src/app/(navbar)/search/_component/PackagesList.tsx
@@ -6,13 +6,14 @@ import type { PackageInfo } from "@/app/types";
 import Package from "./Package";
 
 const PackagesList = () => {
-  const { data, isLoading } = usePackageListQuery();
+  const { data, isLoading, isError, error } = usePackageListQuery();
 
   if (isLoading) return <div>스켈렙톤 할 수 있으까,,</div>;
+  if (isError) return <div>{error.message}</div>;
 
   return (
     <div className="h-[263px] overflow-auto whitespace-nowrap last:mr-0">
-      {data?.data.map((item: PackageInfo) => (
+      {data?.data.data.map((item: PackageInfo) => (
         <Package key={item.packageId} data={item} />
       ))}
     </div>
