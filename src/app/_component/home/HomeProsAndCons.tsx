0fbeb5e77d28a2dd9a8b30548ab687ecdd6a1ed1
applevalley <skatkd7571@naver.com>
2024-01-24 14:39:41 +0900

    feat: 메인 찬반토론 API 연결

--- src/app/_component/home/HomeProsAndCons.tsx
+++ src/app/_component/home/HomeProsAndCons.tsx
@@ -1,51 +1,58 @@
 "use client";
 
-import getPolls from "@/api/home/getPolls";
-import Button from "@/app/_component/common/atom/Button";
-import { useEffect, useState } from "react";
+// import getPolls from "@/api/home/getPolls";
+// import Button from "@/app/_component/common/atom/Button";
+import useGetPollsMainQuery from "@/hooks/query/useGetPollsMainQuery";
+// import { useEffect, useState } from "react";
 
-interface Props {
-  alreadySubmitted: boolean;
-  subject: string;
-  pollId: number;
-  A: string[];
-  B: string[];
-}
+// interface Props {
+//   alreadySubmitted: boolean;
+//   subject: string;
+//   pollId: number;
+//   A: string[];
+//   B: string[];
+// }
 
 const HomeProsAndCons = () => {
-  const [polls, setPolls] = useState<Props[]>();
+  // const [polls, setPolls] = useState<Props[]>();
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await getPolls();
-        setPolls(response);
-      } catch (error) {
-        console.log(error);
-      }
-    };
+  const { data: polls } = useGetPollsMainQuery();
 
-    fetchData();
-    // cleanup
-    return () => {};
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  console.log(polls);
 
-  return (
-    // TODO: 찬반토론 이미지 넘어오는대로 추가 수정 예정입니다!
-    <div>
-      {polls?.map((poll) => (
-        <div className="w-full relative" key={`poll-${poll.pollId}`}>
-          <Button
-            text={`${poll.A.map((letter) => letter).join("")} VS ${poll.B.map(
-              (letter) => letter,
-            ).join(" ")}`}
-            styleClass="w-full h-20 bg-pink font-bold rounded-lg"
-          />
-        </div>
-      ))}
-    </div>
-  );
+  return <div>123</div>;
+
+  // useEffect(() => {
+  //   const fetchData = async () => {
+  //     try {
+  //       const response = await getPolls();
+  //       setPolls(response);
+  //       console.log(response);
+  //     } catch (error) {
+  //       console.log(error);
+  //     }
+  //   };
+
+  //   fetchData();
+  //   // cleanup
+  //   return () => {};
+  // }, []);
+
+  // return (
+  //   // TODO: 찬반토론 이미지 넘어오는대로 추가 수정 예정입니다!
+  //   <div>
+  //     {polls?.map((poll) => (
+  //       <div className="w-full relative" key={`poll-${poll.pollId}`}>
+  //         <Button
+  //           text={`${poll.A.map((letter) => letter).join("")} VS ${poll.B.map(
+  //             (letter) => letter,
+  //           ).join(" ")}`}
+  //           styleClass="w-full h-20 bg-pink font-bold rounded-lg"
+  //         />
+  //       </div>
+  //     ))}
+  //   </div>
+  // );
 };
 
 export default HomeProsAndCons;
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,36 +1,69 @@
+import {
+  HydrationBoundary,
+  QueryClient,
+  dehydrate,
+} from "@tanstack/react-query";
+import getPollsMain from "@/api/balance/getPollsMain";
+
 import BottomNav from "./_component/common/layout/BottomNav";
 import DefaultHeader from "./_component/common/layout/DefaultHeader";
 import HomeAdvertisements from "./_component/home/HomeAdvertisements";
 import HomeHashtags from "./_component/home/HomeHashtags";
 // import HomePackages from "./_component/home/HomePackages";
-// import HomeProsAndCons from "./_component/home/HomeProsAndCons";
+import HomeProsAndCons from "./_component/home/HomeProsAndCons";
 import HomeThemePackage from "./_component/home/HomeThemePackage";
 import ContentsContainer from "./_component/common/layout/ContentsContainer";
 
 const Home = async () => {
+  const queryClient = new QueryClient();
+
+  // await queryClient.prefetchQuery({
+  //   queryKey: [],
+  //   queryFn: ,
+  // })
+  // await queryClient.prefetchQuery({
+  //   queryKey: [],
+  //   queryFn: ,
+  // })
+  //   await queryClient.prefetchQuery({
+  //   queryKey: [],
+  //   queryFn: ,
+  // })
+  //   await queryClient.prefetchQuery({
+  //   queryKey: [],
+  //   queryFn: ,
+  // })
+  await queryClient.prefetchQuery({
+    queryKey: ["polls", "main"],
+    queryFn: getPollsMain,
+  });
+  const dehydrateState = dehydrate(queryClient);
+
   return (
     <>
       <main>
         <section className="w-full">
           <DefaultHeader text="/" theme="main" />
-          <div className="flex flex-col items-center">
-            {/* 광고구좌 */}
-            <HomeAdvertisements />
-            {/* 해시태그 검색 */}
-            <HomeHashtags />
-            {/* 패키지 컨셉 */}
-            <ContentsContainer title="여행 테마 패키지">
-              <HomeThemePackage />
-            </ContentsContainer>
-            {/* 찬반토론 */}
-            {/* <ContentsContainer title="찬반 토론 참여">
-              <HomeProsAndCons />
-            </ContentsContainer> */}
-            {/* 초특가 패키지 목록 */}
-            {/* <ContentsContainer title="지금 핫한 초특가 상품">
-              <HomePackages />
-            </ContentsContainer> */}
-          </div>
+          <HydrationBoundary state={dehydrateState}>
+            <div className="flex flex-col items-center">
+              {/* 광고구좌 */}
+              <HomeAdvertisements />
+              {/* 해시태그 검색 */}
+              <HomeHashtags />
+              {/* 패키지 컨셉 */}
+              <ContentsContainer title="여행 테마 패키지">
+                <HomeThemePackage />
+              </ContentsContainer>
+              {/* 찬반토론 */}
+              <ContentsContainer title="찬반 토론 참여">
+                <HomeProsAndCons />
+              </ContentsContainer>
+              {/* 초특가 패키지 목록 */}
+              {/* <ContentsContainer title="지금 핫한 초특가 상품">
+                <HomePackages />
+              </ContentsContainer> */}
+            </div>
+          </HydrationBoundary>
         </section>
       </main>
       <BottomNav />
