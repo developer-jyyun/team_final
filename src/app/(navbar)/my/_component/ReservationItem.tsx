73006cf1f33a20ac665f26587281f1e84f4a608f
developer-jyyun <developer.jyyun@gmail.com>
2024-01-24 22:51:18 +0900

    Feat: 리뷰작성 기간에 따른 list 렌더링 조건 변경

--- src/app/(navbar)/my/_component/ReservationItem.tsx
+++ src/app/(navbar)/my/_component/ReservationItem.tsx
@@ -74,8 +74,8 @@ const ReservationItem = ({
     );
   }
 
-  // 리뷰 작성 불가
-  if (orderData.isReviewed || !canWriteReview) {
+  // 리뷰 작성 불가 탭
+  if (theme === "reservationTab" && (orderData.isReviewed || !canWriteReview)) {
     return (
       <li className=" mb-6 w-full h-[90px] relative flex flex-row gap-1 justify-evenly web:h-[120px]">
         <div className="w-[90px] shrink-0 rounded-lg overflow-hidden web:w-[120px]">
@@ -101,7 +101,6 @@ const ReservationItem = ({
               />
             )}
           </div>
-
           {orderData.isReviewed && (
             <Button
               text="리뷰 작성 완료"
@@ -116,6 +115,49 @@ const ReservationItem = ({
               disabled
             />
           )}
+        </div>
+      </li>
+    );
+  }
+
+  // 리뷰 작성 불가 메뉴
+  if (
+    theme === "reservationMenu" &&
+    (orderData.isReviewed || !canWriteReview)
+  ) {
+    return (
+      <li className="mb-6 w-full h-[90px] relative  flex flex-row gap-1 justify-evenly web:h-[120px]">
+        <div className="w-[90px] shrink-0 rounded-lg overflow-hidden web:w-[120px]">
+          <img
+            className="w-full h-full"
+            src={orderData.imageUrl}
+            alt={orderData.title}
+          />
+        </div>
+        <div className="w-2/3  flex flex-col justify-evenly gap-2 ">
+          <p className="flex flex-row items-center max-w-[90%] ">
+            <span className="font-medium text-lg text-black-2 leaing-3 truncate">
+              {orderData.title}
+            </span>
+          </p>
+          <div className="">
+            {hashTag && <Chip chipData={orderData.hashtags} gap="gap-x-1" />}
+          </div>
+
+          <div className="flex justify-start items-center text-[11px] gap-2 web:gap-6">
+            <span className="text-red-1 text-xxs web:text-sm ">
+              {orderData.lodgeDays}박 {orderData.tripDays}일
+            </span>
+            <span className="text-red-1 text-xs font-medium web:text-sm ">
+              {orderData.travelPeriod}
+            </span>
+            <Link href={`/items/${orderData.packageId}`}>
+              <Button
+                text="전체 보기"
+                styleClass="rounded-lg text-xs py-1 px-2 bg-pink-main text-white mb-0.5"
+              />
+            </Link>
+          </div>
         </div>
       </li>
     );
