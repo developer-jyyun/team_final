20e2fd791025ce5b57db5e8e747e3e9f2ca988a1
sunpaaaa <sunpaaaa@gmail.com>
2024-01-25 12:39:35 +0900

    Fix: 무한스크롤 수정

--- src/app/(navbar)/heart/_component/WishesList.tsx
+++ src/app/(navbar)/heart/_component/WishesList.tsx
@@ -27,21 +27,26 @@ const WishesList = ({
     isFetching,
     hasNextPage,
   );
-  console.log(data?.pages);
+
   return (
-    <div>
-      <ul className={"flex flex-col gap-6"}>
-        {data?.pages[0].data.map((wish: PackageInfo) => (
-          <Wish key={wish.packageId} data={wish} />
-        ))}
-      </ul>
+    <>
+      {data?.pages.map((page, pageIndex) => {
+        return (
+          <ul key={pageIndex} className={"flex flex-col gap-6"}>
+            {page.data.map((wish: PackageInfo, index: number) => (
+              <Wish key={wish.packageId + index} data={wish} />
+            ))}
+          </ul>
+        );
+      })}
+
       <li
         ref={lastElementRef}
         className="w-full text-center text-black-6 h-20 list-none"
       >
         {isFetching && <div>loading..🎈</div>}
       </li>
-    </div>
+    </>
   );
 };
 
