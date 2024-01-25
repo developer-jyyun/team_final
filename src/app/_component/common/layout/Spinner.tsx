f7dea6e1933eb5fc47d741a55e09752fc6b58097
sunpaaaa <sunpaaaa@gmail.com>
2024-01-26 02:55:12 +0900

    Feat: 로딩 스피너 추가

---
+++ src/app/_component/common/layout/Spinner.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Spinner = () => {
+  return (
+    <div className="w-16 h-16 border-t-4 border-pink-main border-solid rounded-full animate-spin" />
+  );
+};
+
+export default Spinner;
