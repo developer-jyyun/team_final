c513de507a4d04d6c741e84453692be8adef4e6d
applevalley <skatkd7571@naver.com>
2024-01-25 10:48:24 +0900

    fix: 초특가 패키지 쿼리 속성 옵셔널 변경

--- src/hooks/query/usePackageListQuery.tsx
+++ src/hooks/query/usePackageListQuery.tsx
@@ -2,13 +2,13 @@ import getSalePackages from "@/api/home/getSalePackages";
 import { useQuery } from "@tanstack/react-query";
 
 const usePackageListQuery = (
-  page: number,
-  nation: string,
-  continent: string,
+  page?: number,
+  nation?: string,
+  continent?: string,
 ) => {
   return useQuery({
     queryKey: ["packages", page, nation],
-    queryFn: () => getSalePackages(page, nation, continent),
+    queryFn: () => getSalePackages(page!, nation!, continent!),
   });
 };
 
