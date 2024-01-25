2ed99a10cb2a30e3c903991dd588742d97020494
sunpaaaa <sunpaaaa@gmail.com>
2024-01-26 00:55:20 +0900

    Design: 검색페이지 스켈렙톤 디테일 수정

--- src/app/(navbar)/search/_component/Skeleption.tsx
+++ src/app/(navbar)/search/_component/Skeleption.tsx
@@ -4,19 +4,19 @@ const SkeletonItem = () => {
   return (
     <div className="inline-block mr-3">
       <div className="w-[158px] relative">
-        <div className="h-[180px] w-full bg-gray-300 rounded-[12px] animate-pulse" />
+        <div className="h-[180px] w-full bg-grey-d rounded-[12px] animate-pulse" />
       </div>
       <div className="w-[150px] my-1 ml-0.5 flex flex-col justify-between gap-1">
-        <div className="w-full h-4 bg-gray-300 animate-pulse rounded-md" />
+        <div className="w-full h-4 bg-grey-d animate-pulse rounded-md" />
         <div className="flex gap-1 ">
           {[1, 2, 3].map((index) => (
             <div
               key={index}
-              className="h-4 w-8 bg-gray-300 animate-pulse rounded-md"
+              className="h-4 w-8 bg-grey-d animate-pulse rounded-md"
             />
           ))}
         </div>
-        <div className="text-sm text-gray-300 font-extrabold h-4 w-20 bg-gray-300 animate-pulse rounded-md" />
+        <div className="h-4 w-20 bg-grey-d animate-pulse rounded-md" />
       </div>
     </div>
   );
