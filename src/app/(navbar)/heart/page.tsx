b8626d146109d6c4ba0e50dd21d34177a62a365c
sunpaaaa <sunpaaaa@gmail.com>
2024-01-26 03:22:29 +0900

    Feat: 찜목록에서 링크 버튼 연결

--- src/app/(navbar)/heart/page.tsx
+++ src/app/(navbar)/heart/page.tsx
@@ -15,7 +15,7 @@ const HeartPage = () => {
   return (
     <div className="w-full flex flex-col items-center">
       <DefaultHeader text="찜리스트" />
-      <div className="w-full h-full px-6 flex flex-col">
+      <section className="w-full h-full px-6 flex flex-col">
         <p className="my-6 text-lg font-semibold leading-normal tracking-tighter text-left self-start">
           총 <span className=" text-pink-main">{page?.totalElements}</span>
           개의 패키지 상품
@@ -23,9 +23,8 @@ const HeartPage = () => {
         {!page?.totalElements ? (
           <div className="h-full flex flex-col justify-between">
             <EmptyContainer text="찜한 상품이 없습니다." />
-            {/* 추후 Link에 href 제대로 연결해야해요!!!!!! */}
             <Link
-              href={"/"}
+              href={"/theme/0"}
               className="rounded-xl my-6 bg-pink text-white text-lg font-semibold py-2 px-16 cursor-pointer"
             >
               다양한 상품 둘러보러 가기
@@ -39,7 +38,7 @@ const HeartPage = () => {
             hasNextPage={hasNextPage}
           />
         )}
-      </div>
+      </section>
     </div>
   );
 };
