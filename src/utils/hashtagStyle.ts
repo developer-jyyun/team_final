b59094bf34b3a17a0315bb71e7bc025804846cba
sunpaaaa <sunpaaaa@gmail.com>
2024-01-25 23:13:03 +0900

    Move: 필요없는 파일 삭제

--- src/utils/hashtagStyle.ts
+++
@@ -1,8 +0,0 @@
-const hashtagStyle = (rank: number) => {
-  const fontSize = `text-[${10 + (10 - rank)}px]`;
-  const fontWeight = rank < 3 ? "font-bold" : "font-normal";
-
-  return { fontSize, fontWeight };
-};
-
-export default hashtagStyle;
