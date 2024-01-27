db08acf491f708bdd87b6e31e967c1b2626103c1
sunpaaaa <sunpaaaa@gmail.com>
2024-01-27 13:05:02 +0900

    Refactor: 검색 필터 페이지 버튼 항상 하단에 노출

--- src/app/(navbar)/search-filter/_component/SearchButton.tsx
+++ src/app/(navbar)/search-filter/_component/SearchButton.tsx
@@ -1,27 +1,60 @@
 "use client";
 
 import Link from "next/link";
-import React from "react";
+import React, { useEffect, useState } from "react";
+import useScrollUp from "@/hooks/useScrollUp";
 import useSearchCount from "../_hooks/useSearchCount";
 
 const SearchButton = () => {
   const { data, params } = useSearchCount();
   const count = data?.data.count;
 
+  const isScrollUp = useScrollUp();
+  const [isScrolling, setIsScrolling] = useState(false);
+
+  useEffect(() => {
+    let scrollTimeout: NodeJS.Timeout;
+
+    const handleScroll = () => {
+      setIsScrolling(true);
+
+      clearTimeout(scrollTimeout);
+      scrollTimeout = setTimeout(() => {
+        setIsScrolling(false);
+      }, 200);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
+
+  const setPosition = () => {
+    if (isScrollUp || !isScrolling) return "60px";
+    return "4px";
+  };
+
   return (
-    <button
-      type="button"
-      disabled={count !== 0}
-      className={`h-12 w-[327px] ${
-        count === 0 ? `bg-grey-e text-grey-8` : `bg-pink-main text-white`
-      } rounded-[80px]`}
+    <div
+      className={`sticky w-full flex justify-center`}
+      style={{ bottom: setPosition() }}
     >
-      <Link href={`/search-result${params}`} className="w-full">
-        <p className="font-medium text-lg">
-          검색된 <span className="font-extrabold">{count}</span>개의 상품 보기
-        </p>
-      </Link>
-    </button>
+      <button
+        type="button"
+        disabled={count !== 0}
+        className={`h-12 w-[327px] ${
+          count === 0 ? `bg-grey-e text-grey-8` : `bg-pink-main text-white`
+        } rounded-[80px]`}
+      >
+        <Link href={`/search-result${params}`} className="w-full">
+          <p className="font-medium text-lg">
+            검색된 <span className="font-extrabold">{count}</span>개의 상품 보기
+          </p>
+        </Link>
+      </button>
+    </div>
   );
 };
 
