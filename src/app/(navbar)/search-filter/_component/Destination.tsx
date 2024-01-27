bada790325006d258ae296a3e078a4e9392dae8b
sunpaaaa <sunpaaaa@gmail.com>
2024-01-27 22:16:15 +0900

    Design: 해시태그 검색 페이지 디자인 수정

--- src/app/(navbar)/search-filter/_component/Destination.tsx
+++ src/app/(navbar)/search-filter/_component/Destination.tsx
@@ -27,16 +27,16 @@ const Destination = ({ data, type }: Props) => {
     setSelected((prev) => !prev);
   };
   return (
-    <div className="w-[98px]" onClick={handleClick}>
+    <div className="w-[98px] web:w-[130px]" onClick={handleClick}>
       <div
-        className={`w-full h-[100px] py-2.5 px-[9px] bg-grey-e rounded-[12px] ${
+        className={`w-full h-[100px] web:h-[130px] py-2.5 px-[9px] bg-grey-e rounded-[12px] flex justify-center items-center ${
           selected && "opacity-50"
         }`}
       >
         <img
           src={data.imageUrl}
           alt={`${data.name} 이미지`}
-          className="w-20 h-20"
+          className="w-20 h-20 web:w-24 web:h-24"
         />
       </div>
       <p className="text-base font-medium leading-5 tracking-tighter text-center py-2">
